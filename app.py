98ff4932b9a9ad6816554a9d224c5948ae11e0ea
ADITYA KUMAR <adityakumar99hell@gmail.com>
2025-10-05 09:58:53 +0530

    5-10-25

--- app.py
+++ app.py
@@ -7,25 +7,39 @@ import torch.nn.functional as F
 from torchvision import transforms, models
 from torchvision.models import MobileNet_V3_Large_Weights
 
-app = FastAPI() 
-templates = Jinja2Templates(directory="templates")
+app = FastAPI() #app is your FastAPI application.
+templates = Jinja2Templates(directory="templates") #templates tells FastAPI that your HTML files are stored in the folder named “templates”.
 
 # model setup
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-weights = MobileNet_V3_Large_Weights.DEFAULT
-model = models.mobilenet_v3_large(weights=weights)
+weights = MobileNet_V3_Large_Weights.DEFAULT #load up the default weights for our model.
+model = models.mobilenet_v3_large(weights=weights) #now create the model with those (trained) weights.
 model.eval().to(device)
 
-# ✅ Get ImageNet class labels
-imagenet_labels = weights.meta["categories"]
+"""""device → decides whether to use GPU (cuda) or CPU.
 
-preprocess = transforms.Compose([
+weights → load pretrained weights for MobileNetV3.
+
+model → create the MobileNetV3 model.
+
+model.eval() → put it in evaluation mode (no training).
+
+to(device) → move the model to GPU if available.
+
+This means the model is ready to classify images."""
+
+# ✅ Get ImageNet class labels
+imagenet_labels = weights.meta["categories"] #To get all of the labels.
+
+# The transformation is based on the these different transforms.
+preprocess = transforms.Compose([ 
     transforms.Resize(256),
     transforms.CenterCrop(224),
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.485, 0.456, 0.406],
                          std =[0.229, 0.224, 0.225]),
 ])
+
 
 @app.get("/", response_class=HTMLResponse)
 def home(request: Request):
@@ -36,18 +50,19 @@ async def predict(request: Request, file: UploadFile = File(...)):
     img_bytes = await file.read()
     img = Image.open(io.BytesIO(img_bytes)).convert("RGB")
 
-    x = preprocess(img).unsqueeze(0).to(device)
+    x = preprocess(img).unsqueeze(0).to(device) #transform the image to the tensor with other transformations
     with torch.no_grad():
-        logits = model(x)
-        probs = F.softmax(logits, dim=1)
-        topk = torch.topk(probs, k=5)
+        logits = model(x) #get the logits, by passing the inputs to our model.
+        probs = F.softmax(logits, dim=1) #get the probabilities rather than logits
+        topk = torch.topk(probs, k=5) #get the top 5 most likely classes
 
     top_probs = topk.values[0].cpu().tolist()
     top_idxs  = topk.indices[0].cpu().tolist()
 
     # ✅ Map IDs to human-readable labels
     top_labels = [imagenet_labels[idx] for idx in top_idxs]
-
+    
+    #To convert the img_byts into the Human redable image.
     b64 = base64.b64encode(img_bytes).decode("utf-8")
     mime = file.content_type or "image/jpeg"
     data_url = f"data:{mime};base64,{b64}"
